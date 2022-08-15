df1e14a5025cb4a878473cbb612cf10bdabb9ee4
andherpaulo <andher.dev@gmail.com>
2022-08-15 10:19:10 -0300

    Utilização no projeto dos conceitos aprendidos na aula de sexta-feira 12/08 (this, variáveis globais)

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
-function selectDish(option) {
-    const clickedItem = document.querySelector(option);
-    const selectedDish = document.querySelector('.dishes .is-selected');
-    const selectedDrink = document.querySelector('.drinks .is-selected');
-    const selectedDessert = document.querySelector('.desserts .is-selected');
+let selectedDish = [];
+let selectedDrink = [];
+let selectedDessert = [];
 
-    if (selectedDish !== null) {
-        selectedDish.classList.remove('is-selected');
+function selectDish(option) {
+    if (selectedDish[2] !== undefined) {
+        selectedDish[2].classList.remove('is-selected');
     }
 
-    clickedItem.classList.add('is-selected');
+    option.classList.add('is-selected');
 
-    const updatedDish = document.querySelector('.dishes .is-selected');
-    if (updatedDish !== null && selectedDrink !== null && selectedDessert !== null) {
+    if (selectedDrink[2] !== undefined && selectedDessert[2] !== undefined) {
         activateFooterBtn();
     }
+
+    selectedDish = [document.querySelector('.dishes .is-selected h3'), document.querySelector('.dishes .is-selected span'), document.querySelector('.dishes .is-selected')];
+
+    console.log(selectedDish);
 }
 
 function selectDrink(option) {
-    const clickedItem = document.querySelector(option);
-    const selectedDish = document.querySelector('.dishes .is-selected');
-    const selectedDrink = document.querySelector('.drinks .is-selected');
-    const selectedDessert = document.querySelector('.desserts .is-selected');
-
-    if (selectedDrink !== null) {
-        selectedDrink.classList.remove('is-selected');
+    if (selectedDrink[2] !== undefined) {
+        selectedDrink[2].classList.remove('is-selected');
     }
 
-    clickedItem.classList.add('is-selected');
+    option.classList.add('is-selected');
 
-    const updatedDrink = document.querySelector('.drinks .is-selected');
-
-    if (selectedDish !== null && updatedDrink !== null && selectedDessert !== null) {
+    if (selectedDish[2] !== undefined && selectedDessert[2] !== undefined) {
         activateFooterBtn();
     }
+
+    selectedDrink = [document.querySelector('.drinks .is-selected h3'), document.querySelector('.drinks .is-selected span'), document.querySelector('.drinks .is-selected')];
 }
 
 function selectDessert(option) {
-    const clickedItem = document.querySelector(option);
-    const selectedDish = document.querySelector('.dishes .is-selected');
-    const selectedDrink = document.querySelector('.drinks .is-selected');
-    const selectedDessert = document.querySelector('.desserts .is-selected');
-
-    if (selectedDessert !== null) {
-        selectedDessert.classList.remove('is-selected');
+    if (selectedDessert[2] !== undefined) {
+        selectedDessert[2].classList.remove('is-selected');
     }
 
-    clickedItem.classList.add('is-selected');
+    option.classList.add('is-selected');
 
-    const updatedDessert = document.querySelector('.desserts .is-selected');
-    if (selectedDish !== null && selectedDrink !== null && updatedDessert !== null) {
+    if (selectedDish[2] !== undefined && selectedDrink[2] !== undefined) {
         activateFooterBtn();
     }
+
+    selectedDessert = [document.querySelector('.desserts .is-selected h3'), document.querySelector('.desserts .is-selected span'), document.querySelector('.desserts .is-selected')];
 }
 
 function activateFooterBtn() {
@@ -58,6 +51,7 @@ function activateFooterBtn() {
     btn.innerHTML = 'Fechar pedido';
     btn.classList.add('c-btn-active');
 }
+let total = 0;
 
 function checkRequest() {
     const ready = document.querySelector('.c-btn-active');
@@ -65,11 +59,7 @@ function checkRequest() {
     if (ready !== null) {
         const confirmation = document.querySelector('.confirmation');
         confirmation.classList.toggle('is-disabled');
-
-        const selectedDish = [document.querySelector('.dishes .is-selected h3'), document.querySelector('.dishes .is-selected span')];
-        const selectedDrink = [document.querySelector('.drinks .is-selected h3'), document.querySelector('.drinks .is-selected span')];
-        const selectedDessert = [document.querySelector('.desserts .is-selected h3'), document.querySelector('.desserts .is-selected span')];
-
+        
         const displayedDish = [document.querySelector('.confirmation--food p:first-child'), document.querySelector('.confirmation--food p:last-child')];
         const displayedDrink = [document.querySelector('.confirmation--drink p:first-child'), document.querySelector('.confirmation--drink p:last-child')];
         const displayedDessert = [document.querySelector('.confirmation--dessert p:first-child'), document.querySelector('.confirmation--dessert p:last-child')];
@@ -85,7 +75,7 @@ function checkRequest() {
         displayedDessert[0].innerHTML = selectedDessert[0].innerHTML;
         displayedDessert[1].innerHTML = selectedDessert[1].innerHTML;
 
-        const total = dishPrice + drinkPrice + dessertPrice;
+        total = dishPrice + drinkPrice + dessertPrice;
         const displayedTotal = document.querySelector('.confirmation--total span');
         displayedTotal.innerHTML = total.toFixed(2).replace('.', ',');
     }
@@ -94,17 +84,12 @@ function checkRequest() {
 function finishRequest() {
     const name = prompt('Qual é o seu nome?');
     const address = prompt('Qual é o seu endereço?');
-    const total = document.querySelector('.confirmation--total span').innerHTML.replace(',', '.');
-    const selectedDish = document.querySelector('.dishes .is-selected h3').innerHTML;
-    const selectedDrink = document.querySelector('.drinks .is-selected h3').innerHTML;
-    const selectedDessert = document.querySelector('.desserts .is-selected h3').innerHTML;
 
-
-    window.open('https://wa.me/?text=' + encodeURIComponent(`Olá, gostaria de fazer o pedido:
-- Prato: ${selectedDish}
-- Bebida: ${selectedDrink}
-- Sobremesa: ${selectedDessert}
-Total: R$ ${total}
+    window.open('https://wa.me/5534992919292?text=' + encodeURIComponent(`Olá, gostaria de fazer o pedido:
+- Prato: ${selectedDish[0].innerHTML}
+- Bebida: ${selectedDrink[0].innerHTML}
+- Sobremesa: ${selectedDessert[0].innerHTML}
+Total: R$ ${total.toFixed(2)}
 
 Nome: ${name}
 Endereço: ${address}`), '_blank').focus();
